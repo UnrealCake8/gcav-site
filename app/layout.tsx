1487a7b3446f09b6d02d896690b80b03169c2d70
H A <unrealcake8andfriends@proton.me>
2026-08-18 16:25:41 +0400

    Load shock homepage styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./cinematic.css";
 import "./pages.css";
 import "./experiences.css";
 import "./game-experiences.css";
+import "./shock.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { InteractiveFX } from "@/components/InteractiveFX";
